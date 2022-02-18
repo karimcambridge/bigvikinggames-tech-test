084a9cd43cac0c5ad2f90f4ad5753905c08529c2
Kar <4283893+karimcambridge@users.noreply.github.com>
2022-02-18 15:15:03 -0400

    feat: knock out a few tasks
    - A left click must reveal a tile, if not flagged.
    - When a player is actively playing and the mouse is down: the smiley will be in limbo (face_limbo).
    - A tile will not be revealed by any means (middle click or left click) if the tile is flagged.
    - A right click must flag a tile or unflag a tile if already flagged.
    - A timer will keep track of the how long the user has been playing. It begins ticking once the first tile is revealed. This is essentially the player score.

--- Software Developer Challenge - 2021/Challenges/minesweeper/js/minesweeper.js
+++ Software Developer Challenge - 2021/Challenges/minesweeper/js/minesweeper.js
@@ -1,93 +1,188 @@
+const
+	settings = {
+		difficulties: [
+			{
+				id: 0,
+				name: 'easy',
+				cols: 9,
+				rows: 9,
+				mines: 10,
+			},
+			{
+				id: 1,
+				name: 'medium',
+				cols: 16,
+				rows: 16,
+				mines: 40,
+			},
+			{
+				id: 2,
+				name: 'hard',
+				cols: 30,
+				rows: 16,
+				mines: 99,
+			},
+		],
+		states: {
+			UNKNOWN: 'UNKNOWN',
+			FLAGGED: 'FLAGGED',
+			REVEALED: 'REVEALED',
+			EXPLODE: 'EXPLODE',
+			QUESTION: 'QUESTION',
+		},
+		timerPad: 3,
+	},
+	gameOptions = {
+		data: {
+			timerInterval: null,
+		},
+		time: 0,
+		difficulty: settings.difficulties.find(d => d.name === 'easy'),
+		state: {
+			game: false,
+			remainingMines: 0,
+		},
+	}
+;
 
-var time = 0;
+window.addEventListener('DOMContentLoaded', event => {
+	document.getElementById('timer').innerHTML = gameOptions.time?.toString()?.padStart(settings.timerPad, '0');
+	document.getElementById('flagCount').innerHTML = gameOptions.difficulty.mines?.toString()?.padStart(settings.timerPad, '0');
+});
 
-function buildGrid() {
+window.addEventListener('mousedown', e => {   
+	if(e.button === 0 && document.getElementById('minefield')?.contains(e.target)) { // clicked in minefield
+		const smiley = document.getElementById('smiley');
+		smiley.classList.add('face_limbo');
+	}
+});
 
-    // Fetch grid and clear out old elements.
-    var grid = document.getElementById("minefield");
-    grid.innerHTML = "";
+window.addEventListener('mouseup', e => {   
+	// mouse dragged anywhere
+	if(e.button === 0) {
+		const smiley = document.getElementById('smiley');
+		if(smiley.classList.contains('face_limbo')) {
+			smiley.classList.remove('face_limbo');
+		}
+	}
+});
 
-    var columns = 1;
-    var rows = 1;
+const buildGrid = () => {
+	// Fetch grid and clear out old elements.
+	const
+		grid = document.getElementById('minefield'),
+		columns = gameOptions.difficulty.cols,
+		rows = gameOptions.difficulty.rows
+	;
+	grid.innerHTML = '';
 
-    // Build DOM Grid
-    var tile;
-    for (var y = 0; y < rows; y++) {
-        for (var x = 0; x < columns; x++) {
-            tile = createTile(x,y);
-            grid.appendChild(tile);
-        }
-    }
-    
-    var style = window.getComputedStyle(tile);
+	// Build DOM Grid
+	let tile;
+	for(let y = 0; y < rows; y++) {
+		for(let x = 0; x < columns; x++) {
+			tile = createTile(x, y);
+			grid.appendChild(tile);
+		}
+	}
+	const style = window.getComputedStyle(tile);
 
-    var width = parseInt(style.width.slice(0, -2));
-    var height = parseInt(style.height.slice(0, -2));
-    
-    grid.style.width = (columns * width) + "px";
-    grid.style.height = (rows * height) + "px";
-}
+	const width = parseInt(style.width.slice(0, -2));
+	const height = parseInt(style.height.slice(0, -2));
 
-function createTile(x,y) {
-    var tile = document.createElement("div");
+	grid.style.width = (columns * width) + 'px';
+	grid.style.height = (rows * height) + 'px';
+};
 
-    tile.classList.add("tile");
-    tile.classList.add("hidden");
-    
-    tile.addEventListener("auxclick", function(e) { e.preventDefault(); }); // Middle Click
-    tile.addEventListener("contextmenu", function(e) { e.preventDefault(); }); // Right Click
-    tile.addEventListener("mouseup", handleTileClick ); // All Clicks
+const createTile = (x, y) => {
+	const tile = document.createElement('div');
 
-    return tile;
-}
+	tile.classList.add('tile');
+	tile.classList.add('hidden');
+	
+	tile.addEventListener('auxclick', e => { e.preventDefault(); }); // Middle Click
+	tile.addEventListener('contextmenu', e => { e.preventDefault(); }); // Right Click
+	tile.addEventListener('mouseup', handleTileClick ); // All Clicks
 
-function startGame() {
-    buildGrid();
-    startTimer();
-}
+	const mine = document.createAttribute('data-mine');       
+	mine.value = 'false';
+	tile.setAttributeNode(mine);
+	return tile;
+};
 
-function smileyDown() {
-    var smiley = document.getElementById("smiley");
-    smiley.classList.add("face_down");
-}
+const startGame = () => {
+	buildGrid();
+};
 
-function smileyUp() {
-    var smiley = document.getElementById("smiley");
-    smiley.classList.remove("face_down");
-}
+const smileyDown = () => {
+	const smiley = document.getElementById('smiley');
+	smiley.classList.add('face_down');
+};
 
-function handleTileClick(event) {
-    // Left Click
-    if (event.which === 1) {
-        //TODO reveal the tile
-    }
-    // Middle Click
-    else if (event.which === 2) {
-        //TODO try to reveal adjacent tiles
-    }
-    // Right Click
-    else if (event.which === 3) {
-        //TODO toggle a tile flag
-    }
-}
+const smileyUp = () => {
+	const smiley = document.getElementById('smiley');
+	smiley.classList.remove('face_down');
+};
 
-function setDifficulty() {
-    var difficultySelector = document.getElementById("difficulty");
-    var difficulty = difficultySelector.selectedIndex;
+const handleTileClick = event => {
+	console.log('mouse click:' + event.which);
+	const
+		tile = event.target
+	;
+	if(gameOptions.state.game === false) {
+		gameOptions.state.game = true;
+		startOver();
+		console.log(gameOptions.difficulty);
+	}
+	if(!tile.classList.contains('hidden')) {
+		return;
+	}
+	// Left Click
+	if(event.which === 1) { // reveal a tile
+		if(tile.classList.contains('flag')) return;
+		tile.classList.remove('hidden');
+	}
+	// Middle Click
+	else if(event.which === 2) {
+		if(tile.classList.contains('flag')) return;
+		//TODO try to reveal adjacent tiles
+	}
+	// Right Click
+	else if(event.which === 3) { // toggle a tile flag
+		//TODO toggle a tile flag
+		if(tile.classList.contains('flag')) {
+			tile.classList.remove('flag');
+		} else {
+			tile.classList.add('flag');
+		}
+	}
+};
 
-    //TODO implement me
-}
+const setDifficulty = () => {
+	const
+		difficultySelector = document.getElementById('difficulty')
+	;
+	//console.log(difficultySelector.options);
+	gameOptions.difficulty = settings.difficulties[difficultySelector.selectedIndex];
+	//TODO implement me
+};
 
-function startTimer() {
-    timeValue = 0;
-    window.setInterval(onTimerTick, 1000);
-}
+const startOver = () => {
+	gameOptions.time = 0;
+	if(gameOptions.data.timerInterval !== null) {
+		window.clearInterval(gameOptions.data.timerInterval);
+	}
+	gameOptions.data.timerInterval = window.setInterval(onTimerTick, 1000);
+	updateTimer();
 
-function onTimerTick() {
-    timeValue++;
-    updateTimer();
-}
+	gameOptions.state.remainingMines = gameOptions.difficulty.mines;
+	document.getElementById('flagCount').innerHTML = gameOptions.state.remainingMines?.toString()?.padStart(settings.timerPad, '0');
+};
 
-function updateTimer() {
-    document.getElementById("timer").innerHTML = timeValue;
-}
+const onTimerTick = () => {
+	gameOptions.time++;
+	updateTimer();
+};
+
+const updateTimer = () => {
+	document.getElementById('timer').innerHTML = gameOptions.time?.toString()?.padStart(settings.timerPad, '0');
+};
